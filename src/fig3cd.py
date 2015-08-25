9a3c924a1626b404d7291bd665df08c54ae25bb0
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-08-25 23:59:31 +0200

    Minor changes.

--- src/fig3cd.py
+++ src/fig3cd.py
@@ -1,2 +1,20 @@
 #! /usr/bin/python
 
+def execute_command(command):
+	sys.stderr.write('[%s wrapper] %s\n' % (MAPPER_NAME, command));
+	subprocess.call(command, shell=True);
+	sys.stderr.write('\n');
+
+def execute_command_get_stdout(command):
+	sys.stderr.write('[%s wrapper] %s\n' % (MAPPER_NAME, command));
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	[out, err] = p.communicate()
+	sys.stderr.write('\n');
+
+	return [out, err];
+
+def main():
+	pass;
+
+if __name__ == "__main__":
+	main()
